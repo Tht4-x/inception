4d6d582f893ec262b118d7bd7be218b798898cb8
Tht4-x <dorianancel@hotmail.fr>
2026-05-14 21:51:20 +0200

    gdg

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,10 +4,12 @@ set -e
 DB_PASSWORD=$(cat /run/secrets/db_password)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-mysqld_safe --skip-networking &
-sleep 3
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
-mysql -u root <<EOF
+    mysqld_safe --skip-networking &
+    sleep 3
+
+    mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
@@ -15,6 +17,11 @@ GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
+    mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
+    echo "MariaDB initialized successfully"
 
-echo "MariaDB initialized successfully"
+else
+    echo "MariaDB already initialized, skipping..."
+fi
+
+exec mysqld_safe
